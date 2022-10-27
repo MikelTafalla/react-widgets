a4978fd795beaac07d3de2a18b1e3d9e3fc794d2
Mikel <mikel362d@gmail.com>
2022-10-26 18:14:53 -0700

    adds routing

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
+import Route from './components/Route';
 
 const items = [
     {
@@ -18,7 +19,6 @@ const items = [
         content: 'You use React by creating components'
     }
 ];
-
 const options = [
     {
         label: 'Dawn orange',
@@ -35,9 +35,27 @@ const options = [
 ];
 
 const App = () => {
+    const [selected, setSelected] = useState(options[0]);
+
     return (
         <div>
-            <Translate />
+            <Route path="/">
+                <Accordion items={items} />
+            </Route>
+            <Route path="/list">
+                <Search />
+            </Route>
+            <Route path="/dropdown">
+                <Dropdown 
+                    label="Select a color"
+                    options={options}
+                    selected={selected}
+                    onSelectedChange={setSelected}
+                />
+            </Route>
+            <Route path="/translate">
+                <Translate />
+            </Route>
         </div>
     );
 };
